f17b36d7ad0ebc56c6525aa3005ea3a4bea9b02e
cameronbaffuto <cb3426@drexel.edu>
2022-09-22 18:16:30 -0400

    space

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -213,9 +213,9 @@ function Home() {
         
           <h5 className="my-3 mx-3">Number of Drugs: {amount}</h5>
           <p className="my-3 mx-3">Day: {daytime} | Night: {nighttime} | Both: {bothdn} | As Needed: {asneed}</p>
-          
-          <Stack className="mr-4" direction="horizontal" gap={3}>
-            <TextField dark autofocus bordered type="text" label="Search..." value={query} onChange={(e) => setQuery(e.value)} /> 
+
+          <TextField dark autofocus bordered type="text" label="Search..." value={query} onChange={(e) => setQuery(e.value)} /> 
+          <Stack className="mx-4" direction="horizontal" gap={3}>
             <Fab dark onClick={handleShowFilter}> <BsFillFilterCircleFill style={{ color: '#fff', fontSize: '20px'}}/> </Fab>
             <Fab dark onClick={resetFilter}><RiFilterOffFill style={{ color: '#fff', fontSize: '20px'}}/></Fab> 
           </Stack>
